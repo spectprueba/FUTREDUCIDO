dd343480326745c16c02bfc5e10012c10cf8a456
spectprueba <93741795+spectprueba@users.noreply.github.com>
2023-08-25 18:48:03 -0600

    Mega update
    Cositas

---
+++ js/loader.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    var container = document.getElementById('loader_container');
+    //var loaderContainer = document.querySelector('.loader');
+
+    var wholeDoc = document.querySelector('html');
+    wholeDoc.style.overflowY = 'hidden';
+
+
+    setTimeout(function() {
+
+        wholeDoc.style.overflowY = 'visible';
+        container.style.width = '0%';
+
+    }, 900);
+
+    setTimeout(function() {
+
+        container.style.opacity = '0';
+
+    }, 850);
+
+    setTimeout(function() {
+
+        container.style.visibility = 'hidden';
+        container.style.display = 'none';
+
+    }, 1700);
+
+}
